06faae4af5cfcaf309d96f4754f1946f98ae491b
Rafiikhwan <rafiikhwan2006@gmail.com>
2023-09-22 00:28:56 +0700

    Dashboard Page and Navigation Component

--- app/src/main/java/com/nandanarafiardika/aplikasispp/LoginActivity.java
+++ app/src/main/java/com/nandanarafiardika/aplikasispp/LoginActivity.java
@@ -2,13 +2,26 @@ package com.nandanarafiardika.aplikasispp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText et_username, et_password;
+    private Button btn_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+        btn_submit  = (Button) findViewById(R.id.btn_login_submit);
+
+        btn_submit.setOnClickListener(view -> {
+            Intent i = new Intent(this, DashboardActivity.class);
+            startActivity(i);
+        });
     }
 }
